008ac44ada02ea63631e3380967b2c9df91d72b3
jackmqiu <jackqiu2016@gmail.com>
2018-02-14 11:54:05 -0800

    webpack add

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'client');
+
+var config = {
+    context: path.join(__dirname),
+    entry: 'index',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                include: APP_DIR,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader|css-loader?-svgo'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        modules: [
+            path.join(__dirname, 'client'),
+            'node_modules'
+        ]
+    }
+};
+
+module.exports = config;

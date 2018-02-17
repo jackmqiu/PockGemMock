d290ad5ec632413563a9b271b438ba43a71f4604
jackmqiu <jackqiu2016@gmail.com>
2018-02-16 16:09:49 -0800

    RewardBranch uses scss file

--- client/components/RewardBranch.js
+++ client/components/RewardBranch.js
@@ -1,16 +1,17 @@
 import React from "react";
+import styles from "../style/RewardBranch.scss";
 
 const RewardCategory = ({ branch, onBranchSelect }) => {
   const imageUrl = branch.imageUrl;
 
   return (
-    <li onClick={() => onCategorySelect(video)} className="reward-category">
+    <li onClick={() => onBranchSelect(video)} className="reward-branch">
       <div className="video-list media">
         <div className="media-left">
           <img className="branch-image" src={branch.imageUrl} />
         </div>
         <div className="media-body">
-          <div className="media-heading">{branch.title}</div>
+          <div className={styles.reward__branch_heading}>{branch.title}</div>
         </div>
       </div>
     </li>

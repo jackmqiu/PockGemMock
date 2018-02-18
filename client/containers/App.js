dd190ab1602f1ea4110f0188998e0e2d61bd4598
jackmqiu <jackqiu2016@gmail.com>
2018-02-18 15:55:42 -0800

    index.jsx uses react-router, App made into container

---
+++ client/containers/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Counter from '../components/Counter';
+import NavBar from './NavBar';
+import Season from './Season';
+import RewardList from './RewardList';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from "../reducers";
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render () {
+    return (
+      <div>
+        <NavBar/>
+
+      </div>
+    )
+  }
+}
+
+export default App;
--- client/index.jsx
+++ client/index.jsx
@@ -4,33 +4,10 @@ import ReactDOM from 'react-dom';
 import { browserHistory, Router, Route, Link, withRouter } from 'react-router-3';
 import routes from './routes';
 
-import Counter from './components/Counter';
-import NavBar from './components/NavBar';
-import Season from './containers/Season';
-import RewardList from './containers/RewardList';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from "./reducers";
 
-import {SeasonObject, Rewards} from './stateMock.js';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      season: SeasonObject,
-      rewards: Rewards,
-    };
-  }
-  render () {
-    return (
-      <div>
-        <NavBar />
-
-      </div>
-    )
-  }
-}
 
 
 ReactDOM.render(

452d05a1f071fa20a1573178d99fe9fafb838258
jackmqiu <jackqiu2016@gmail.com>
2018-02-19 01:41:55 -0800

    Conditional button displaying

--- client/containers/RewardList.js
+++ client/containers/RewardList.js
@@ -1,12 +1,31 @@
 import React, {Component} from "react";
-import RewardListItem from "../components/RewardListItem.js";
+import RewardListItem from "../components/RewardListItem";
 import styles from "../style/RewardList.scss";
+import buttonStyles from "../style/Buttons.scss";
 import { connect } from 'react-redux';
 
 class RewardList extends Component {
   constructor(props) {
     super(props);
+    let rewardPages = this.dividePages(this.props.rewards[this.props.activeBranch.key]);
+
+    this.state = {
+      rewardsPages: rewardPages,
+      activePage: 1,
+    }
   }
+
+  dividePages(rewards) {
+
+    let rewardPagesArr = [];
+    if(rewards) {
+      for (let i = 0; i < rewards.length / 28; i++) {
+        rewardPagesArr.push(rewards.slice(i * 28, i * 28 + 28));
+      }
+    }
+    return rewardPagesArr;
+  }
+
   renderRewards(reward) {
     return (
       <RewardListItem
@@ -18,10 +37,24 @@ class RewardList extends Component {
 
   render() {
     return (
-      <div className={styles.season_container}>
-        {this.props.rewards[this.props.activeBranch.key].map(
-          (reward) => this.renderRewards(reward)
-        )}
+      <div className={styles.container}>
+
+        {this.state.activePage < this.state.rewardsPages.length &&
+          <button className={buttonStyles.next_button}>
+            Next
+          </button>
+        }
+        {this.state.activePage > 1 &&
+          <button className={buttonStyles.next_button}>
+            Previous
+          </button>
+        }
+        <div>Page {this.state.activePage} of {this.state.rewardsPages.length}</div>
+        <div className={styles.season_container}>
+          {this.state.rewardsPages[this.state.activePage - 1].map(
+            (reward) => this.renderRewards(reward)
+          )}
+        </div>
       </div>
     );
   }

cf41e7c0c1c649540dfb3dda5351065106f93c23
jackmqiu <jackqiu2016@gmail.com>
2018-02-19 12:22:33 -0800

    Reward Branch converted to React Component to include handleInfoClick

--- client/components/RewardBranch.js
+++ client/components/RewardBranch.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, {Component} from "react";
 import styles from "../style/RewardBranch.scss";
+import buttonStyles from "../style/Buttons.scss";
+import { connect } from 'react-redux';
+import { toggleModal } from "../actions/index";
+import { bindActionCreators } from "redux";
 
-const RewardCategory = ({ branch, onBranchSelect }) => {
-  const imageUrl = branch.imageUrl;
 
-  return (
-    <div>
+class RewardCategory extends Component {
+  constructor(props) {
+    super(props);
+  }
+  handleInfoClick(branch) {
+    this.props.toggleModal(branch);
+  }
+  render(){
+    const imageUrl = this.props.branch.imageUrl;
+    let modalBranch = this.props.branch;
+    console.log(modalBranch);
+    return (
+      <div>
       <div className={styles.reward__branch_slot}>
-        <figure>
-          <img className={styles.branch_image} src={branch.imageUrl} />
-        </figure>
-        <div className={styles.reward__branch_heading}>{branch.title}</div>
+      <figure>
+      <img className={styles.branch_image} src={this.props.branch.imageUrl} />
+      </figure>
+      <div className={styles.reward__branch_heading}>{this.props.branch.title}</div>
+      {this.props.branch.lockStatus &&
+        <button
+        className={buttonStyles.lock_button}
+        onClick={() => this.handleInfoClick(this.props.branch)}
+        >
+        Info
+        </button>
+      }
       </div>
-    </div>
-  );
+      </div>
+    );
+  }
 };
 
-export default RewardCategory;
+function mapStateToProps(state) {
+  return {
+
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ toggleModal: toggleModal }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RewardCategory);

97021469086977691b787ae1bcf5abb80456c0b4
jackmqiu <jackqiu2016@gmail.com>
2018-02-18 22:11:29 -0800

    modified Redux store in index to use Chrome redux devTools

--- client/index.jsx
+++ client/index.jsx
@@ -8,10 +8,13 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from "./reducers";
 
-
+const store = createStore(
+   reducers,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes = {routes}/>
 
   </Provider>

531731e00ce1c0dc25c72ac2d4277e507ba5279a
jackmqiu <jackqiu2016@gmail.com>
2018-02-19 13:27:10 -0800

    made modalStatus update into text so modal could be universal

--- client/components/RewardBranch.js
+++ client/components/RewardBranch.js
@@ -11,28 +11,27 @@ class RewardCategory extends Component {
     super(props);
   }
   handleInfoClick(branch) {
-    this.props.toggleModal(branch);
+    this.props.toggleModal(branch.unlockCriteria);
   }
   render(){
     const imageUrl = this.props.branch.imageUrl;
-    let modalBranch = this.props.branch;
-    console.log(modalBranch);
+
     return (
       <div>
-      <div className={styles.reward__branch_slot}>
-      <figure>
-      <img className={styles.branch_image} src={this.props.branch.imageUrl} />
-      </figure>
-      <div className={styles.reward__branch_heading}>{this.props.branch.title}</div>
-      {this.props.branch.lockStatus &&
-        <button
-        className={buttonStyles.lock_button}
-        onClick={() => this.handleInfoClick(this.props.branch)}
-        >
-        Info
-        </button>
-      }
-      </div>
+        <div className={styles.reward__branch_slot}>
+          <figure>
+            <img className={styles.branch_image} src={this.props.branch.imageUrl} />
+          </figure>
+        <div className={styles.reward__branch_heading}>{this.props.branch.title}</div>
+        {this.props.branch.lockStatus &&
+          <button
+          className={buttonStyles.lock_button}
+          onClick={() => this.handleInfoClick(this.props.branch)}
+          >
+            Info
+          </button>
+        }
+        </div>
       </div>
     );
   }

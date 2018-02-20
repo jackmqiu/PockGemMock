3b06844c7bbee2f5d912e222b2e2b33a6349257d
jackmqiu <jackqiu2016@gmail.com>
2018-02-20 06:16:35 -0800

    armoury picture set

--- client/components/Armoury.js
+++ client/components/Armoury.js
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "../style/RewardListItem.scss";
 
 const Armoury = ({ reward, onRewardSelect }) => {
-  const defaultImgUrl = "https://www.deckshop.pro/img/card/InfernoD.png";
+  const defaultImgUrl = "http://www.lfcbootroom.net/wp-content/uploads/2017/12/video-comercial-novo-clash-royale.jpg";
   return (
     <div onClick={() => onRewardSelect(reward)}>
       <figure>
-        <img className={styles.reward_image} src={defaultImgUrl} />
+        <img className={styles.reward_image} src={"https://i.imgur.com/ziUxAyf.png" || defaultImgUrl} />
       </figure>
     </div>
   );

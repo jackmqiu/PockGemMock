32388b4426bf2aebb007c5f844dbe26e274ad506
jackmqiu <jackqiu2016@gmail.com>
2018-02-19 22:56:32 -0800

    filler Component Battle

---
+++ client/components/Battle.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../style/RewardListItem.scss";
+
+const Battle = ({ reward, onRewardSelect }) => {
+  const defaultImgUrl = "https://www.deckshop.pro/img/card/InfernoD.png";
+  return (
+    <div onClick={() => onRewardSelect(reward)}>
+      <figure>
+        <img className={styles.reward_image} src={defaultImgUrl} />
+      </figure>
+    </div>
+  );
+};
+
+export default Battle;

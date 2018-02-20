0e3f3f8edbd20aa9c170f9aa4b9684f6d204a672
jackmqiu <jackqiu2016@gmail.com>
2018-02-20 06:16:44 -0800

    custom pictures set

--- client/components/Battle.js
+++ client/components/Battle.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../style/RewardListItem.scss";
 
 const Battle = ({ reward, onRewardSelect }) => {
-  const defaultImgUrl = "https://www.deckshop.pro/img/card/InfernoD.png";
+  const defaultImgUrl = "https://i.ytimg.com/vi/4Jdp_dkduJc/maxresdefault.jpg";
   return (
     <div onClick={() => onRewardSelect(reward)}>
       <figure>
